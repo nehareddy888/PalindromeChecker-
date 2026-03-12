44dd650606c66d8923354e6956cc8654c161170b
unknown <ms5456@srmist.edu.in>
2026-03-12 15:46:26 +0530

    [nehareddyv] Added UC2-PrintHardcodedPalindromeResult

--- APP/src/Palindromeapp.java
+++ APP/src/Palindromeapp.java
@@ -1,13 +1,31 @@
 public class Palindromeapp
 {
     public static void main(String[] args) {
+
+        String word = "madam";
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
         System.out.println();
         System.out.println("--------------------------");
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0 Palindrome Checker Management System");
-        System.out.println("Version 1.0 initialized successfully.");
-        System.out.println("System initialized successfully.");
+        System.out.println("Palindrome Checker - UC2");
+        System.out.println("--------------------------");
+        System.out.println("Checking word: " + word);
+
+        if (isPalindrome) {
+            System.out.println("Result: \"" + word + "\" IS a palindrome.");
+        } else {
+            System.out.println("Result: \"" + word + "\" is NOT a palindrome.");
+        }
+
+        System.out.println("--------------------------");
         System.out.println();
-        // Program continues to next use case or exits.
     }
 }
