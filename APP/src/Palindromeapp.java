5bef12ebf223ba5f6465465e498a69340ad7524b
unknown <ms5456@srmist.edu.in>
2026-03-12 15:26:16 +0530

    [nehareddyv] Added UC1-ApplicationEntryWelcomeMessage

---
+++ APP/src/Palindromeapp.java
@@ -0,0 +1,13 @@
+public class Palindromeapp
+{
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("--------------------------");
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0 Palindrome Checker Management System");
+        System.out.println("Version 1.0 initialized successfully.");
+        System.out.println("System initialized successfully.");
+        System.out.println();
+        // Program continues to next use case or exits.
+    }
+}
